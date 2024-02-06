c0f5338d439a1ab26e1990fb5b0d6f6c8844d850
MaartjeFe <134467938+MaartjeFe@users.noreply.github.com>
2024-02-06 11:16:21 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
-function showMessage() {
-    alert('Hello, world!');
+// Get the button and popup
+var button = document.getElementById("popupButton");
+var popup = document.getElementById("popup");
+
+// When the user clicks the button, open the popup
+button.onclick = function() {
+  popup.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the popup
+document.getElementsByClassName("close")[0].onclick = function() {
+  popup.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the popup, close it
+window.onclick = function(event) {
+  if (event.target == popup) {
+    popup.style.display = "none";
+  }
 }
